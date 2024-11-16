043e19928b96e9ba58d703cfc7c5624950709da1
ani <yichatma@gmail.com>
2024-11-17 02:17:12 +0800

    Remove anygrasp

--- yc_ws/src/app/app/__init__.py
+++
@@ -1,8 +0,0 @@
-# app/__init__.py
-from .coordinator import Coordinator
-from .grasp_detection import GraspDetector
-from .grasp_detection_controller import GraspDetectorController
-from .camera_interface import CameraInterface
-from .camera_interface_controller import CameraInterfaceController
-#from .anygrasp_sdk.grasp_detection.demo import GraspDetector
-from .grasp_detection import GraspDetector2
