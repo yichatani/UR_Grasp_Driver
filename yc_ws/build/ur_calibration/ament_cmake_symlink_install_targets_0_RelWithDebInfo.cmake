57fb61adab00af9de010bb2df06768e580436032
Yicheng Ma <156786736+yichatani@users.noreply.github.com>
2024-12-05 01:39:16 +0800

    Delete yc_ws/build directory

--- yc_ws/build/ur_calibration/ament_cmake_symlink_install_targets_0_RelWithDebInfo.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/home/ani/yc_ws/build/ur_calibration/calibration_correction" "TARGETS" "calibration_correction" "RUNTIME_DESTINATION" "lib/ur_calibration")
